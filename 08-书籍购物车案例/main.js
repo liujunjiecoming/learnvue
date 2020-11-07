c2a260233003b3ab003adcd75259fad360172d00
JJLiu <13431967860@163.com>
2020-11-07 11:33:31 +0800

    06条件判断；07遍历循环；08书籍购物车案例

---
+++ 08-书籍购物车案例/main.js
@@ -0,0 +1,64 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    books: [
+      {
+        id: 1,
+        name: '《Java》',
+        date: '2006-9',
+        price: 85.00,
+        count: 1
+      },
+      {
+        id: 2,
+        name: '《Python》',
+        date: '2006-2',
+        price: 59.00,
+        count: 1
+      },
+      {
+        id: 3,
+        name: '《Php》',
+        date: '2006-5',
+        price: 67.00,
+        count: 1
+      },
+      {
+        id: 4,
+        name: '《Vue》',
+        date: '2006-11',
+        price: 45.00,
+        count: 1
+      },
+    ]
+  },
+  methods: {
+    getFinalPrice(price) {
+      return '¥' + price.toFixed(2);
+    },
+    increment(index) {
+      this.books[index].count++;
+    },
+    decrement(index) {
+      this.books[index].count--;
+    },
+    removeHandle(index) {
+      this.books.splice(index, 1);
+    }
+  },
+  computed: {
+    totalPrice() {
+      let totalPrice = 0;
+      for (let i = 0; i < this.books.length; i++) {
+        totalPrice += this.books[i].price * this.books[i].count
+      }
+      return totalPrice;
+    }
+  },
+  //过滤器的使用
+  filters: {
+    showPrice(price) {
+      return '¥' + price.toFixed(2);
+    }
+  }
+})
